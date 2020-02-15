14c5252e2b5c93bd61738bbdd75fdb0137aa6a95
Vinu Sagar <vinusagar@live.in>
2020-02-15 20:05:27 +0530

    Initial Commit

---
+++ src/main/java/com/notyfyd/SpringDataJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.notyfyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notyfyd/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notyfyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
