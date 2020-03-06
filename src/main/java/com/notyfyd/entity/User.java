fb88d07bbdb8e44d54c591ed792f00488965acf1
Vinu Sagar <vinusagar@live.in>
2020-03-06 18:22:04 +0530

    Initial Commit - JPA - part 3 - Initial

--- src/main/java/com/notyfyd/entity/User.java
+++ src/main/java/com/notyfyd/entity/User.java
@@ -1,5 +1,4 @@
 package com.notyfyd.entity;
-
 import javax.persistence.*;
 
 @Entity
@@ -13,54 +12,42 @@ public class User {
     private String mobile;
     @Column(unique = true)
     private String email;
-
     @ManyToOne
     private Role role;
 
     public Role getRole() {
         return role;
     }
-
     public void setRole(Role role) {
         this.role = role;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getMobile() {
         return mobile;
     }
-
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
